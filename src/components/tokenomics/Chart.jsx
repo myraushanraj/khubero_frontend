50290fd4065638b83d2f58ebaa75bd7145a5656f
myraushanraj <raushan10695@gmail.com>
2022-07-22 18:03:18 +0530

    bug fix

--- src/components/tokenomics/Chart.jsx
+++ src/components/tokenomics/Chart.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Doughnut } from 'react-chartjs-2'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip);
 
 export const data = {
   labels: ['Token Supply', 'Marketing & development', 'Diluted market cap', 'Reinvested', 'Legal', 'Locked tokens'],
